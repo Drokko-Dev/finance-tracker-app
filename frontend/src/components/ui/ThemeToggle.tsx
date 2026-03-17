19d5f5964b4015f983f54de66a8c5aa5550f5d41
Drokko-Dev <jaime.vegah@outlook.com>
2026-03-17 01:27:24 -0300

    changes in toggletheme

--- frontend/src/components/ui/ThemeToggle.tsx
+++ frontend/src/components/ui/ThemeToggle.tsx
@@ -1,31 +1,34 @@
 import { Sun, Moon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-export const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(() => {
-    // Al cargar, revisa si el usuario ya tenía una preferencia guardada
-    return document.documentElement.classList.contains("dark");
-  });
+const iconBtnStyles =
+  "cursor-pointer w-10 h-10 rounded-full border border-[var(--color-border-subtle)] flex justify-center items-center transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5 hover:bg-slate-50 dark:hover:bg-white/5";
 
-  useEffect(() => {
-    if (isDark) {
+export const ThemeToggle = ({ className }: { className: string }) => {
+  // Leemos directamente del DOM que ya fue procesado por el script de arriba
+  const [isDark, setIsDark] = useState(() =>
+    document.documentElement.classList.contains("dark"),
+  );
+
+  const toggleTheme = () => {
+    const newDark = !isDark;
+    setIsDark(newDark);
+
+    if (newDark) {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
     }
-  }, [isDark]);
+  };
 
   return (
-    <button
-      onClick={() => setIsDark(!isDark)}
-      className="cursor-pointer w-10 h-10 rounded-xl border border-[var(--color-border-subtle)] flex justify-center items-center transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5 hover:bg-slate-50 dark:hover:bg-white/5"
-    >
+    <button onClick={toggleTheme} className={className}>
       {isDark ? (
-        <Sun className="w-5 h-5 text-yellow-400" />
+        <Sun className="w-[18px] h-[18px]" />
       ) : (
-        <Moon className="w-5 h-5 text-indigo-600" />
+        <Moon className="w-[18px] h-[18px]" />
       )}
     </button>
   );
